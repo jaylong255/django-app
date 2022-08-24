8a63d730b339596ba1725e810040e0b35c361272
Jay <jaylong255@gmail.com>
2022-08-24 11:56:01 -0500

    adding postgres config to settings

--- hellodjango/hellodjango/settings.py
+++ hellodjango/hellodjango/settings.py
@@ -84,8 +84,13 @@ else:
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DB_FILEPATH,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+        'ATOMIC_REQUESTS': True
     }
 }
 
